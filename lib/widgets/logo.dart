04fe63ee248b12858536ab4b1b9283ea7b2ea5ba
hideokawahara <kawahide.72@gmail.com>
2022-09-12 22:05:44 +0900

    feat: ロゴ作成

---
+++ lib/widgets/logo.dart
@@ -0,0 +1,39 @@
+//Packages
+import 'package:flutter/material.dart';
+
+class TopLogo extends StatelessWidget {
+  const TopLogo({
+    Key? key,
+    this.width = 400,
+    this.height = 200,
+  }) : super(key: key);
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    //Todo: ロゴは暫定版
+    return Container(
+      width: width,
+      height: height,
+      color: Colors.grey[200],
+      alignment: Alignment.center,
+      child: const Text(
+        'TATSUJIN GUILD',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.w900,
+          decoration: TextDecoration.overline,
+          shadows: <Shadow>[
+            Shadow(
+              color: Colors.grey,
+              offset: Offset(2.0, 5.0),
+              blurRadius: 3.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
