d8023da64fea8ba297e8c2f596329f5cb4ff753e
hideokawahara <kawahide.72@gmail.com>
2022-09-19 16:05:52 +0900

    feat: 投稿ボタンのテスト追加

---
+++ test/view_models/time_line_view_model_test.dart
@@ -0,0 +1,36 @@
+//Packages
+import 'package:flutter_test/flutter_test.dart';
+
+//Models
+import 'package:tatsujin_guild/models/post.dart';
+
+//ViewModels
+import 'package:tatsujin_guild/view_models/time_line_view_model.dart';
+
+void main() {
+  final timeLineViewModel = TimeLineViewModel();
+  group('取得のテスト', () {
+    test('ランキングと投稿の取得', () async {
+      expect(timeLineViewModel.rankingList.isEmpty, true);
+      expect(timeLineViewModel.timeLineList.isEmpty, true);
+      await timeLineViewModel.fetchInitialAllPosts();
+      expect(timeLineViewModel.rankingList.isNotEmpty, true);
+      expect(timeLineViewModel.timeLineList.isNotEmpty, true);
+    });
+  });
+
+  group('投稿ボタンのテスト', () {
+    test('like counterの有効時のテスト', () {
+      for (int i = 0; i < 5; i++) {
+        if (i < 3) {
+          expect(timeLineViewModel.isShow, false);
+          expect(timeLineViewModel.likeCounter, i);
+        } else if (i >= 3) {
+          expect(timeLineViewModel.isShow, true);
+          expect(timeLineViewModel.likeCounter, i);
+        }
+        timeLineViewModel.addLikeCounter();
+      }
+    });
+  });
+}
