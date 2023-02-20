3ef702da3ee77952ed2d26387676affdc2a2aaa9
hideokawahara <kawahide.72@gmail.com>
2023-02-20 22:16:14 +0900

    feat: mock追加

--- test/repositories/auth_repository_test.dart
+++ test/repositories/auth_repository_test.dart
@@ -1,5 +1,7 @@
 //Packages
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 //Models
 import 'package:tatsujin_guild/models/user.dart';
@@ -7,12 +9,25 @@ import 'package:tatsujin_guild/models/user.dart';
 //Repositories
 import 'package:tatsujin_guild/repositories/auth_repository.dart';
 
+//Test
+import 'auth_repository_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AuthRepositoryImpl>()])
 void main() {
-  final authRepository = AuthRepositoryImpl();
+  final authRepository = MockAuthRepositoryImpl();
   group('認証のテスト', () {
     test('ログイン情報取得', () async {
+      when(authRepository.getMyData()).thenAnswer((_) => Future.value(testUser));
       final User? user = await authRepository.getMyData();
       expect(user != null, true);
     });
   });
 }
+
+final User testUser = User.fromJSON({
+  'name': '達人太郎子',
+  'profile_message':
+  'はじめまして！プロフィールを見て頂きありがとうございます。\n大阪在住の〇〇（名前）といいます。\n結婚を考えていた相手とお別れしたことをきっかけにアプリを始めました！',
+  'main_photo':
+  'https://news.mynavi.jp/article/20211022-1984461/ogp_images/ogp.jpg',
+});
